30eeb31e4ad819a29067a2e3daa3fa2be80683e6
AbdoWise-z <moh2002.abdow@gmail.com>
2025-02-14 17:10:22 +0200

    progress par

---
+++ src/progress_bar.cpp
@@ -0,0 +1,188 @@
+//
+// Created by xabdomo on 2/14/25.
+//
+
+#include "progress_bar.h"
+
+#include <iostream>
+#include <chrono>
+#include <sstream>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <sys/ioctl.h>
+#endif
+
+static int getConsoleWidth() {
+#ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+        return csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    }
+#else
+    struct winsize w{};
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == 0) {
+        return w.ws_col;
+    }
+#endif
+    return 80; // Default width if detection fails
+}
+
+#include <unordered_map>
+#include <regex>
+
+static std::string customFormat(const std::string& format, const std::unordered_map<std::string, std::string>& replacements) {
+    std::string result = format;
+    
+    for (const auto& [key, value] : replacements) {
+        std::regex pattern("%" + key + "%");
+        result = std::regex_replace(result, pattern, value);
+    }
+
+    result = std::regex_replace(result, std::regex(R"(\\%)"), "%");
+
+    return result;
+}
+
+static std::string bar_format;
+static int progress = 0;
+static int max_progress = 100;
+static int bar_size = 25;
+static std::string bar_char = "\u25A0";
+static double speed = 0;
+static std::chrono::steady_clock::time_point start_time;
+static double gamma = 0.4;
+
+
+static void render() {
+    auto screen_width = getConsoleWidth();
+    const auto remaining_time = (max_progress - progress) /  std::max(speed, 0.01);
+    std::ostringstream ss;
+    ss << std::fixed << std::setprecision(2) << speed;
+    const auto spead_str = ss.str();
+    ss.str("");
+
+    ss  << std::dec
+        << static_cast<int>(remaining_time / 60 / 60) << ":"
+        << static_cast<int>(remaining_time / 60) % 60 << ":"
+        << std::fixed << std::setprecision(2)
+        << static_cast<int>(remaining_time) % 60 + (remaining_time - static_cast<int>(remaining_time));
+    const auto time_str = ss.str();
+    ss.str("");
+
+    ss << std::dec << progress;
+    const auto curr_str = ss.str();
+    ss.str("");
+
+    ss << std::dec << max_progress;
+    const auto max_str = ss.str();
+    ss.str("");
+
+    ss << std::fixed << std::setprecision(2) << (100.0 * progress / max_progress);
+    const auto progress_str = ss.str();
+    ss.str("");
+
+    std::unordered_map<std::string, std::string> replacements = {
+        {"spd", spead_str},
+        {"eta", time_str},
+        {"curr", curr_str},
+        {"max", max_str},
+        {"prog", progress_str}
+    };
+
+    auto bar_str = customFormat(bar_format, replacements);
+
+    std::regex pattern("%bar%");
+    if (std::regex_search(bar_str, pattern)) {
+
+        const auto boxes = static_cast<int>(bar_size * progress / max_progress);
+        for (auto i = 0; i < boxes; i++) {
+            ss << bar_char;
+        }
+
+        for (auto i = 0; i < bar_size - boxes; i++) {
+            ss << " ";
+        }
+
+        bar_str = customFormat(bar_str, {
+            {"bar", ss.str()}
+        });
+        ss.str("");
+    }
+
+    std::cout << "\r" << bar_str;
+    std::cout.flush();
+}
+
+// supported marcos:
+// %bar%  : a bar the fills the remainder of the screen.
+// %spd%  : the current speed
+// %eta%  : the expected time to finish [hh:mm:ss.MMM] based on the current spead
+// %curr% : the current progress value
+// %max%  : the max progress value
+// %preg% : the completion percentage
+void progress_bar::set(const std::string &bar, const int max_progress, const int bar_size, const std::string& bar_char) {
+    ::bar_format = bar;
+
+    ::max_progress = max_progress;
+    ::bar_size = bar_size;
+    ::bar_char = bar_char;
+
+    if (::max_progress < 1) {
+        ::max_progress = 1;
+    }
+
+    ::speed = 0;
+    start_time = std::chrono::steady_clock::now();
+    render();
+}
+
+void progress_bar::setMax(int max_progress) {
+    ::max_progress = max_progress;
+}
+
+void progress_bar::setSize(int bar_size) {
+    ::bar_size = bar_size;
+}
+
+void progress_bar::setBarChar(const std::string &bar_char) {
+    ::bar_char = bar_char;
+}
+
+
+void progress_bar::setProgress(int prog) {
+    const auto this_time = std::chrono::steady_clock::now();
+    const auto duration = static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(this_time - start_time).count());
+    start_time = this_time;
+
+    if (prog < 0) prog = 0;
+    if (prog > max_progress) prog = max_progress;
+
+    const auto delta = prog - ::progress;
+    const auto spd = delta / duration * 1E9;
+
+    speed = speed + gamma * (spd - speed); // calculate running average
+
+    ::progress = prog;
+    render();
+}
+
+void progress_bar::reset() {
+    ::progress = 0;
+    ::speed = 0;
+    ::start_time = std::chrono::steady_clock::now();
+}
+
+void progress_bar::hide() {
+    std::cout << "\r";
+}
+
+std::string progress_bar::defaultBarWithTitle(const std::string &title) {
+    return "[" + title + " - %prog%%] |%bar%| %curr%/%max% [%eta%, %spd%it/s]";
+}
+
+
+
+
---
+++ src/progress_bar.h
@@ -0,0 +1,215 @@
+//
+// Created by xabdomo on 2/14/25.
+//
+
+#ifndef PROGRESS_BAR_H
+#define PROGRESS_BAR_H
+
+#include <string>
+#include <utility>
+#include <functional>
+#include <iostream>
+#include <ostream>
+#include <stdexcept>
+
+#define DEFAULT_PROGRESS_BAR "%prog%% |%bar%| %curr%/%max% [%eta%, %spd%it/s]"
+namespace progress_bar {
+
+
+    void set(const std::string &bar, int max_progress = 100, int bar_size = 25, const std::string& bar_char = "\u25A0");
+    void setMax(int max_progress);
+    void setSize(int bar_size);
+    void setBarChar(const std::string &bar_char);
+    void setProgress(int prog);
+    void reset();
+    void hide();
+
+    std::string defaultBarWithTitle(const std::string& title);
+
+
+    template <typename Iterator>
+    class ProgressIterator {
+    public:
+        ProgressIterator(Iterator iter, Iterator end, std::function<void(int)> progressCallback)
+            : iter_(iter), end_(end), progressCallback_(std::move(progressCallback)), index_(0) {}
+
+        auto& operator*() { return *iter_; }
+
+        ProgressIterator& operator++() {
+            ++iter_;
+            ++index_;
+            if (progressCallback_) {
+                progressCallback_(index_);
+            }
+            return *this;
+        }
+
+        ProgressIterator operator++(int) {
+            ProgressIterator temp = *this;
+            ++(*this);
+            return temp;
+        }
+
+        bool operator!=(const ProgressIterator& other) const {
+            return iter_ != other.iter_;
+        }
+
+    private:
+        Iterator iter_;
+        Iterator end_;
+        std::function<void(int)> progressCallback_;
+        int index_;
+    };
+
+    template <typename T>
+    class IterableWrapper {
+    public:
+        explicit IterableWrapper(T& iterable) : iterable_(iterable) {}
+
+        static void progressCallback(int index) {
+            setProgress(index);
+        }
+
+        auto begin() {
+            return ProgressIterator(iterable_.begin(), iterable_.end(),[](const int index) { progressCallback(index); });
+        }
+
+        auto end() {
+            return ProgressIterator(iterable_.end(), iterable_.end(),[](const int index) { progressCallback(index); });
+        }
+
+    private:
+        T& iterable_;
+    };
+
+    template <typename T>
+    IterableWrapper<T> from(T& iterator, int size, const std::string& name = "") {
+        if (name.empty()) {
+            set(DEFAULT_PROGRESS_BAR, size);
+        } else {
+            set(defaultBarWithTitle(name), size);
+        }
+        return IterableWrapper<T>(iterator);
+    }
+
+
+    template <typename T>
+    IterableWrapper<T> from(T& iterator, const std::string& name = "") {
+        const auto size = std::distance(iterator.begin(), iterator.end());
+        if (name.empty()) {
+            set(DEFAULT_PROGRESS_BAR, size);
+        } else {
+            set(defaultBarWithTitle(name), size);
+        }
+        return IterableWrapper<T>(iterator);
+    }
+
+
+    template <typename T>
+    class RangedProgressIterator {
+    public:
+        RangedProgressIterator(T current, T end, T step, std::function<void(int)> progressCallback)
+            : current_(current), end_(end), step_(step), progressCallback_(std::move(progressCallback)), step_prog(1), current_progress(0) {}
+
+        T operator*() const { return current_; }
+
+        RangedProgressIterator& operator++() {
+            current_ += step_;
+
+            if (step_ > 0 && current_ > end_) {
+                current_ = end_ + step_;
+            }
+
+            if (step_ < 0 && current_ < end_) {
+                current_ = end_ + step_;
+            }
+
+            current_progress += step_prog;
+            if (progressCallback_) {
+                progressCallback_(static_cast<int>(current_progress));
+            }
+
+            return *this;
+        }
+
+        RangedProgressIterator operator++(int) {
+            ProgressIterator temp = *this;
+            ++(*this);
+            return temp;
+        }
+
+        bool operator!=(const RangedProgressIterator& other) const {
+            return !(
+                (
+                    (current_ == other.current_) ||
+                    (step_ > 0 && current_ > end_ && other.current_ > other.end_) || // two special conditions for floating numbers
+                    (step_ < 0 && current_ < end_ && other.current_ < other.end_)    // translates to : both out of range.
+                ) &&
+                end_ == other.end_ &&
+                step_ == other.step_
+            );
+        }
+
+    private:
+        T current_;
+        T end_;
+        T step_;
+        long double step_prog;
+        long double current_progress;
+        std::function<void(int)> progressCallback_;
+    };
+
+    template <typename T>
+    class IterableRange {
+    public:
+        IterableRange(T start, T end, T step) : start_(start), end_(end), step_(step) {}
+
+        static void progressCallback(const int index) {
+            setProgress(index);
+        }
+
+        auto begin() {
+            return RangedProgressIterator<T>(start_, end_, step_, [](const int index) { progressCallback(index); });
+        }
+
+        auto end() {
+            return RangedProgressIterator<T>(end_ + step_, end_, step_, [](const int index) { progressCallback(index); });
+        }
+
+    private:
+        T start_;
+        T end_;
+        T step_;
+    };
+
+    template <typename T>
+    IterableRange<T> ranged(T start = 0, T end = 100, T step = 1, const std::string& name = "") {
+
+        if (step == 0) {
+            throw std::invalid_argument("step = 0");
+        }
+
+        if (step > 0) {
+            if (start > end) {
+                throw std::invalid_argument("start > end");
+            }
+        } else {
+            if (start < end) {
+                throw std::invalid_argument("start < end");
+            }
+        }
+
+
+        if (name.empty()) {
+            set(DEFAULT_PROGRESS_BAR, static_cast<int>(std::abs((start - end) / step)));
+        } else {
+            set(defaultBarWithTitle(name), static_cast<int>(std::abs((start - end) / step)));
+        }
+        return IterableRange<T>(start, end, step);
+    }
+
+};
+
+
+
+#endif //PROGRESS_BAR_H
